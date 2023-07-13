a5c6fc30ed8aa8a177b72132be8b0ac7ff24176b
Robdetta <robdetta@gmail.com>
2023-07-13 11:21:56 -0400

    finishing scaffolding

--- server/server.js
+++ server/server.js
@@ -4,16 +4,20 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const path = require('path');
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:8080",
+    }   
+})
 
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {  
-//   res.sendFile(path.join('index.html'));
-// });
+app.get('/', (req, res) => {  
+  res.sendFile(path.join('index.html'));
+});
 
 
 //socket handlers 
@@ -26,7 +30,8 @@ server.listen(3000, () => {
 
 const onConnection = (socket) => {
     registerUserHandlers(io, socket);
-
 }
 
 io.on('connection', onConnection);
+
+
